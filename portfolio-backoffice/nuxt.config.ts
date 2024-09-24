ba8eaa93bc598bbde8a2c49b14ebfd0263d035d2
CarolaneLFBV <carolane_lefebvre@icloud.com>
2024-09-24 16:49:34 +0200

    feat: nuxtui install

--- portfolio-backoffice/nuxt.config.ts
+++ portfolio-backoffice/nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   router: {
     middleware: ['auth']
-  }
+  },
+
+  modules: ['@nuxt/ui']
 })
